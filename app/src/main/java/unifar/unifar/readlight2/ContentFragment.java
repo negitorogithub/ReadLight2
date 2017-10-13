6f023ebd94986e19023727cdd94ea50438dfe8ee
negitorogithub <unifarproject35@gmail.com>
2017-10-13 21:44:33 +0900

    added timer

--- app/src/main/java/unifar/unifar/readlight2/ContentFragment.java
+++ app/src/main/java/unifar/unifar/readlight2/ContentFragment.java
@@ -5,14 +5,20 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import yuku.ambilwarna.colorpicker.AmbilWarnaDialogFragment;
 
@@ -26,6 +32,7 @@ public class ContentFragment extends Fragment implements TimePickerListener{
     private AmbilWarnaDialogFragment ambilWarnaDialogFragment;
     private ViewGroup mcontainer;
     private TimePickerListener msettingButtonListener;
+    private AppCompatActivity mappCompatActivity;
     public ContentFragment() {
         // Required empty public constructor
     }
@@ -45,6 +52,7 @@ public class ContentFragment extends Fragment implements TimePickerListener{
     @Override
     public void onAttach(Context context){
         super.onAttach(context);
+        mappCompatActivity = (AppCompatActivity) context;
         if (context instanceof TimePickerListener){
             this.msettingButtonListener = (TimePickerListener)context;
         }
@@ -58,6 +66,7 @@ public class ContentFragment extends Fragment implements TimePickerListener{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         final ContentFragment thisContext = this;
         this.mcontainer = container;
         int currentColor;
@@ -97,5 +106,38 @@ public class ContentFragment extends Fragment implements TimePickerListener{
     @Override
     public void OnTimeSet(int hour, int minute) {
 
+        Calendar calendar = Calendar.getInstance();
+
+        int hourDelta =0;
+        if (calendar.get(Calendar.HOUR_OF_DAY)<hour) {
+            hourDelta= hour-calendar.get(Calendar.HOUR_OF_DAY);
+        } else if (calendar.get(Calendar.HOUR_OF_DAY)==hour){
+            hourDelta =0;
+        } else if (calendar.get(Calendar.HOUR_OF_DAY)>hour){
+            hourDelta= hour+24-calendar.get(Calendar.HOUR_OF_DAY);
+        }
+        int minuteDelta = 0;
+        if (calendar.get(Calendar.MINUTE)<minute) {
+            minuteDelta= minute-calendar.get(Calendar.MINUTE);
+        } else if (calendar.get(Calendar.MINUTE)==minute){
+            minuteDelta =0;
+        } else if (calendar.get(Calendar.MINUTE)>minute){
+            minuteDelta= minute+60-calendar.get(Calendar.MINUTE);
+        }
+        calendar.add(Calendar.HOUR_OF_DAY, hourDelta);
+        calendar.add(Calendar.MINUTE, minuteDelta);
+        long timeNow = System.currentTimeMillis();
+        long timeToDelay = Math.abs(calendar.getTimeInMillis() - timeNow);
+
+
+
+        new Handler().postDelayed( new Runnable() {
+            @Override
+            public void run() {
+                mappCompatActivity.finish();
+            }
+        },timeToDelay
+        );
+        Log.d("rl2",String.valueOf(timeToDelay));
     }
 }
